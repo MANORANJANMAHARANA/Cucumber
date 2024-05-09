c7753587b69bc9dcb0c8d90b5a4ef448bd206a94
MANORANJANMAHARANA <manoranjan.maharana@gmail.com>
2024-05-09 12:28:32 -0400

    First commit

---
+++ src/main/java/t.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class t {
+
+    public static void main(String args[])
+    {
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "C:\\Users\\Manoranjan PC\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+        // Instantiate a ChromeDriver class.
+        WebDriver driver = new ChromeDriver();
+
+        // Maximize the browser
+        driver.manage().window().maximize();
+
+        // Launch Website
+        driver.get("https://www.geeksforgeeks.org/");
+    }
+}
